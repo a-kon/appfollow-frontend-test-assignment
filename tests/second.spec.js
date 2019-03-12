13475baf38a47238ba561e6ffaef5b35892e3ad3
a-kon <kondratyev.work@gmail.com>
2019-03-12 23:05:50 +0300

    Initialize with 3 tasks

---
+++ tests/second.spec.js
@@ -0,0 +1,18 @@
+const isPangram = require('../src/second.js');
+
+describe('Второе задание', () => {
+	it('Не валидно для пустых строк', () => {
+		expect(isPangram("")).toBe(false);
+	});
+	it('Не валидные значения', () => {
+		expect(isPangram("this isn't a pangram")).toBe(false);
+		expect(isPangram("aaaaaaaaaaaaaaaaaaaaaaaaaa")).toBe(false);
+		expect(isPangram("no it's not")).toBe(false);
+		expect(isPangram("")).toBe(false);
+	});
+	it('Валидные значения', () => {
+		expect(isPangram("the quick brown fox jumps over the lazy dog")).toBe(true);
+		expect(isPangram("cwm fjord bank glyphs vext quiz")).toBe(true);
+		expect(isPangram("pack my box with five dozen liquor jugs")).toBe(true);
+	});
+});
---
+++ tests/third.spec.js
@@ -0,0 +1,30 @@
+const isParentsisValid = require('../src/third.js');
+
+describe('Третье задание', () => {
+	it('Не валидно для строк длиной 1 символ', () => {
+		expect(isParentsisValid("(")).toBe(false);
+		expect(isParentsisValid(")")).toBe(false);
+	});
+	it('Не валидно для строк с нечетным количеством символов', () => {
+		expect(isParentsisValid("(()")).toBe(false);
+		expect(isParentsisValid("()))")).toBe(false);
+		expect(isParentsisValid("()))")).toBe(false);
+		expect(isParentsisValid("(()))))")).toBe(false);
+		expect(isParentsisValid("()))))(")).toBe(false);
+	});
+	it('Валидные значения', () => {
+		expect(isParentsisValid("((((()))))")).toBe(true);
+		expect(isParentsisValid("(()()()())")).toBe(true);
+		expect(isParentsisValid("((())(())(()))")).toBe(true);
+		expect(isParentsisValid("(())((()())())")).toBe(true);
+		expect(isParentsisValid("(())")).toBe(true);
+		expect(isParentsisValid("()")).toBe(true);
+	});
+	it('Не валидные значения', () => {
+		expect(isParentsisValid("()))")).toBe(false);
+		expect(isParentsisValid("(((()))")).toBe(false);
+		expect(isParentsisValid("(((())(())(()))")).toBe(false);
+		expect(isParentsisValid("(()()))")).toBe(false);
+		expect(isParentsisValid("()))")).toBe(false);
+	});
+});
